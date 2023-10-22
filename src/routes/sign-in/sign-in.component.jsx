bece4513c176c8011b3e41abe25b69cc0db35a4a
CarlosSimon02 <caloycamacho02@gmail.com>
2023-10-22 19:45:22 +0800

    Add sign up form component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
-import {getRedirectResult} from 'firebase/auth'
+import { getRedirectResult } from "firebase/auth";
 
 import {
   auth,
   createUserDocumentFromAuth,
   signInWithGooglePopup,
-  signinWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
@@ -14,16 +13,10 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signinWithGoogleRedirect();
-    console.log({user});
-  };
-
   return (
     <div>
       <h1>Sign in Page</h1>
       <button onClick={logGoogleUser}> Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}> Sign in with Google Redirect</button>
     </div>
   );
 };
