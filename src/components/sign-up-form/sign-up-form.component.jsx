74e40638f312e569f8f6f8590940626806b50413
CarlosSimon02 <caloycamacho02@gmail.com>
2023-10-27 20:08:03 +0800

    Define default form field values

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form>
         <label>Display Name</label>
-        <input type="text" required/>
+        <input type="text" required />
         <label>Email</label>
-        <input type="email" required/>
+        <input type="email" required />
         <label>Password</label>
-        <input type="password" required/>
+        <input type="password" required />
         <label>Confirm Password</label>
-        <input type="password" required/>
+        <input type="password" required />
         <button type="submit">Sign Up</button>
       </form>
     </div>
