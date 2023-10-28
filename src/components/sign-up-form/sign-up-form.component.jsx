253def0dd14bcb6128c8485300f78308dad75175
CarlosSimon02 <caloycamacho02@gmail.com>
2023-10-28 20:15:17 +0800

    Define handler for formfields onChange event

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,20 +8,47 @@ const defaultFormFields = {
 };
 
 const SignUpForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const handleChange = (event) => {};
+
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form>
         <label>Display Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          name="displayName"
+          onChange={handleChange}
+          value={displayName}
+        />
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          required
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          name="password"
+          onChange={handleChange}
+          value={password}
+        />
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+        />
         <button type="submit">Sign Up</button>
       </form>
     </div>
