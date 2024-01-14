4fd1ea25dd39092c058b5eeb0c2ffbc02c8c61eb
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-14 10:57:40 +0800

    Add categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
  function App() {
   return (
-    <div className="">
-      Hello World
+    <div className="categories-cotainer">
+      <div className="category-container">
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container">
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container">
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container">
+        <h2>Womens</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container">
+        <h2>Mens</h2>
+        <p>Shop Now</p>
+      </div>
     </div>
   );
 }
