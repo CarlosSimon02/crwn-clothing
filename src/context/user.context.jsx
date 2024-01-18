1320e799fc3a4d61ee6e681712917a97a9a5c785
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-18 21:49:00 +0800

    Use observable pattern to listen any changes in authentication state

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import { onAuthStateChangedListener,createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 // as the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
@@ -10,11 +10,14 @@ export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
-  useEffect(()=> {
-    const unsubscribe = onAuthStateChangedListener((user)=>{
-      setCurrentUser(user)
-    })
-  },[])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+  }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
