dd3ef26d55b1319bc2bda257c138096aa499149c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-19 12:57:26 +0800

    Add product context

---
+++ src/context/products.context.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+import PRODUCTS from "../shop-data.json";
+
+export const ProductContext = createContext({
+  products: [],
+});
+
+export const ProductProvider = ({ children }) => {
+  const { products, setProducts } = useState(PRODUCTS);
+  return <ProductContext.Provider>{children} </ProductContext.Provider>;
+};
